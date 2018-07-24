38a70c506b55109635b330b7d77f6c9ec9cc5393
Jun Omae <jun66j5@gmail.com>
2018-07-24 18:02:24 +0900

    Fix non-PEP440 version string

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='TracHyperestraierPlugin', version='0.1-k-trac0.12',
+    name='TracHyperestraierPlugin', version='1.0.0.0',
     packages=find_packages(exclude=['*.tests*']),
     entry_points = """
         [trac.plugins]
