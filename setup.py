7429ff73c7b6a70ffa9c60b335094c092cdb5103
Jun Omae <jun66j5@gmail.com>
2018-08-02 12:17:30 +0900

    Remove invalid entry points and refactor setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,21 @@
 
 from setuptools import find_packages, setup
 
-setup(
-    name='TracHyperestraierPlugin', version='1.0.0.0',
-    packages=find_packages(exclude=['*.tests*']),
-    entry_points = """
-        [trac.plugins]
-        searchhyperestraier.searchhyperestraier = searchhyperestraier.searchhyperestraier
-        searchhyperestraier.searchattachmenthyperestraier = searchhyperestraier.searchattachmenthyperestraier
-        searchhyperestraier.searchdocumenthyperestraier = searchhyperestraier.searchdocumenthyperestraier
-        searchhyperestraier.searchchangesethyperestraier = searchhyperestraier.searchchangesethyperestraier
-    """,
-)
+def main():
+    entry_points = (
+        'searchhyperestraier.searchhyperestraier',
+    )
+    extra = {}
+
+    setup(
+        name='TracHyperestraierPlugin',
+        version='1.0.0.0',
+        packages=find_packages(exclude=['*.tests*']),
+        entry_points={
+            'trac.plugins': ['%(entry)s = %(entry)s' % dict(entry=entry)
+                             for entry in entry_points],
+        },
+        **extra)
+
+if __name__ == '__main__':
+    main()
